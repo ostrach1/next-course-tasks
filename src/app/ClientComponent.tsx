e7a682610d5b085f93e676870ecfa10509b2d55f
ostrach1 <ostrach109@gmail.com>
2025-04-02 10:43:12 +0200

    remove unused ClientComponent and ServerComponent; update Home component to include content; add .gitignore file

--- src/app/ClientComponent.tsx
+++
@@ -1,3 +0,0 @@
-export default function ClientComponent() {
-  return <div></div>
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className='mt-5 max-w-xl mx-auto'></main>
+  return <main className="mt-5 max-w-xl mx-auto"></main>;
 }
